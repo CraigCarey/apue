eee32f81dd060a095353ba173210a70030b0a441
Craig <sgccarey@gmail.com>
2017-05-14 22:11:42 +0100

    Testing ssh key

--- pluralsight/network/03/rot13client1.c
+++ pluralsight/network/03/rot13client1.c
@@ -9,51 +9,51 @@
 
 main(int argc, char *argv[])
 {
-    struct hostent *host_info;
-    struct sockaddr_in	server;
-    int sock, count;
-    char buf[BSIZE];  /* Copy buffer */
-    char *server_name;
+	struct hostent *host_info;
+	struct sockaddr_in	server;
+	int sock, count;
+	char buf[BSIZE];  /* Copy buffer */
+	char *server_name;
 
-    /* Get server name from command line. If none, use "localhost" */
-    server_name = (argc>1) ? argv[1] : "localhost" ;
+	/* Get server name from command line. If none, use "localhost" */
+	server_name = (argc>1) ? argv[1] : "localhost" ;
 
-    /* Create the socket */
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+	/* Create the socket */
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
 	perror("creating stream socket");
 	exit(1);
-    }
+	}
 
-    /* Look up the host's IP address */
-    host_info = gethostbyname(server_name);
-    if (host_info == NULL) {
+	/* Look up the host's IP address */
+	host_info = gethostbyname(server_name);
+	if (host_info == NULL) {
 	fprintf(stderr, "%s: unknown host: %s\n", argv[0], server_name);
 	exit(2);
-    }
+	}
 
-    /* Set up the server's socket address */
-    server.sin_family = AF_INET;
-    memcpy((char *)&server.sin_addr, host_info->h_addr,
-				     host_info->h_length);
-    server.sin_port = htons(1067); /* rot13 service port */
+	/* Set up the server's socket address */
+	server.sin_family = AF_INET;
+	memcpy((char *)&server.sin_addr, host_info->h_addr,
+					 host_info->h_length);
+	server.sin_port = htons(1067); /* rot13 service port */
 
-    /* Connect to the server */
-    if (connect(sock,(struct sockaddr *)&server,sizeof server) < 0) {
+	/* Connect to the server */
+	if (connect(sock,(struct sockaddr *)&server,sizeof server) < 0) {
 	perror("connecting to server");
 	exit(4);
-    }
+	}
 
-    /* OK now we are connected to the server. Send the user name, then
-       collect  whatever the server  sends back and copy it to stdout.
-    */
+	/* OK now we are connected to the server. Send the user name, then
+	   collect  whatever the server  sends back and copy it to stdout.
+	*/
 
-    printf("connected to server %s\n", server_name);
-    while((count = read(0, buf, BSIZE)) > 0)
-    {
-    write(sock, buf, count);
-    read(sock, buf, count);
-    write(1, buf, count);
-    }
+	printf("connected to server %s\n", server_name);
+	while((count = read(0, buf, BSIZE)) > 0)
+	{
+	write(sock, buf, count);
+	read(sock, buf, count);
+	write(1, buf, count);
+	}
 }
 
